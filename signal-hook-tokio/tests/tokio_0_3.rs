83958b350e00fdf72e7f731facd5ce7caaec16b0
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2020-11-01 13:42:54 +0100

    Implement Tokio 0.3 support
    The implementation provides a wrapper using a Tokio 0.3 UnixStream as
    the self pipe. The wrapper acts as an asynchronous stream of signal
    numbers.

---
+++ signal-hook-tokio/tests/tokio_0_3.rs
@@ -0,0 +1,61 @@
+#![cfg(feature = "support-v0_3")]
+
+use futures_0_3 as futures;
+use tokio_0_3 as tokio;
+
+use futures::stream::StreamExt;
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::time::Duration;
+
+use signal_hook_tokio::v0_3::Signals;
+
+use serial_test::serial;
+
+fn send_sig(sig: libc::c_int) {
+    unsafe { libc::raise(sig) };
+}
+
+#[tokio::test]
+#[serial]
+async fn next_returns_recieved_signal() {
+    let mut signals = Signals::new(&[signal_hook::SIGUSR1]).unwrap();
+    send_sig(signal_hook::SIGUSR1);
+
+    let signal = signals.next().await;
+
+    assert_eq!(signal, Some(signal_hook::SIGUSR1));
+}
+
+#[tokio::test]
+#[serial]
+async fn close_signal_stream() {
+    let mut signals = Signals::new(&[signal_hook::SIGUSR1]).unwrap();
+    signals.handle().close();
+
+    let result = signals.next().await;
+
+    assert_eq!(result, None);
+}
+
+#[tokio::test]
+#[serial]
+async fn delayed() {
+    async fn get_signal(mut signals: Signals, recieved: Arc<AtomicBool>) {
+        signals.next().await;
+        recieved.store(true, Ordering::SeqCst);
+    }
+
+    let signals = Signals::new(&[signal_hook::SIGUSR1]).unwrap();
+    let recieved = Arc::new(AtomicBool::new(false));
+
+    let signals_task = tokio::spawn(get_signal(signals, Arc::clone(&recieved)));
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    assert_eq!(recieved.load(Ordering::SeqCst), false);
+
+    send_sig(signal_hook::SIGUSR1);
+    signals_task.await.unwrap();
+    assert_eq!(recieved.load(Ordering::SeqCst), true);
+}
